52cbeae34c3adb918767d0ff36aa4fc112a4ea5a
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-04-04 15:04:12 +0200

    Update WishlistService.java

--- Cookies-SpringBoot/wishlist-service/src/main/java/com/WishlistService/service/WishlistService.java
+++ Cookies-SpringBoot/wishlist-service/src/main/java/com/WishlistService/service/WishlistService.java
@@ -3,8 +3,11 @@ package com.WishlistService.service;
 import com.WishlistService.model.Wishlist;
 import com.WishlistService.repository.WishlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class WishlistService {
     @Autowired
     private WishlistRepository wishlistRepository;
