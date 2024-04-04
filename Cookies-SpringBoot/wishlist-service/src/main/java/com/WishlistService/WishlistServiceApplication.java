123d8b6473d3975ff85534d4a088585a07db4414
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-04-04 15:04:46 +0200

    Update WishlistServiceApplication.java

--- Cookies-SpringBoot/wishlist-service/src/main/java/com/WishlistService/WishlistServiceApplication.java
+++ Cookies-SpringBoot/wishlist-service/src/main/java/com/WishlistService/WishlistServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.WishlistService"})
 public class WishlistServiceApplication {
 
 	public static void main(String[] args) {
